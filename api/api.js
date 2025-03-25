084fb070920436bd78079c01baec0b76ab2c8cb0
BrunoPa20 <u201914974@upc.edu.pe>
2025-03-25 16:19:20 -0500

    .
    .

--- api/api.js
+++ api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:5162/amsac/v1",
+    baseURL: "http://198.150.0.223:5162/amsac/v1",
     withCredentials: true, // 🔥 Permitir cookies en requests
     headers: {
         "Content-Type": "application/json",
